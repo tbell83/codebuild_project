aef5b0909c331c86a5f929fa335a24be406cd8a2
Thomas Bell <tbell@dramafever.com>
2018-05-10 15:23:53 -0400

    add an output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "deployment_role_arn" {
   value = "${join(",", aws_iam_role.deployment_role.*.arn)}"
 }
 
+output "deployment_role_id" {
+  value = "${join(",", aws_iam_role.deployment_role.*.id)}"
+}
+
 output "project_arn" {
   value = "${join(",", aws_codebuild_project.project.*.id)}"
 }
