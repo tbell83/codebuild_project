7d34e569156ebb7413c9a4e6939cf1f955384e69
Thomas Bell <tbell@dramafever.com>
2018-05-15 09:45:46 -0400

    be a list harder

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_codebuild_project" "project" {
   count        = "${var.count}"
   name         = "${var.name}"
   service_role = "${data.aws_iam_role.deployment_role.arn}"
-  artifacts    = "${list(var.project_artifacts)}"
+  artifacts    = ["${list(var.project_artifacts)}"]
   environment  = "${list(var.project_environment)}"
   source       = "${list(var.project_source)}"
   tags         = "${var.tags}"
